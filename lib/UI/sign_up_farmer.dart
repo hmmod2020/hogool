620422b9cabc2f1dc8cff037afc03a228cd5e420
mohammed albdrie <mstar.hmmod@hotmail.com>
2023-01-10 12:51:34 +0300

    make validation for singUp screen farmer

--- lib/UI/sign_up_farmer.dart
+++ lib/UI/sign_up_farmer.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SingUpFarmer extends StatelessWidget {
+import '../modelView/sing_up_farmer.dart';
 
-  
+class SingUpFarmer extends StatelessWidget {
+var modelView =SignUPFarmerModelView();
+var formKay=GlobalKey<FormState>();
+var testText="hmmod";
 static String singUpFarmer ="/singUpFarmer";
 
   @override
@@ -26,124 +29,21 @@ static String singUpFarmer ="/singUpFarmer";
                  ),),
               ),
               Form(
+                key:formKay,
                 child: Padding(
                   padding: const EdgeInsets.all(25),
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
-                        TextFormField(
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(
-                            hintText: "الإسم",
-                            hintTextDirection: TextDirection.rtl,
-                            filled: true,
-                            fillColor: Color(0xffF0F9F1),
-                            enabledBorder:  OutlineInputBorder( 
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1),
-                              ), 
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            focusedBorder: OutlineInputBorder(  
-                              borderRadius: BorderRadius.all(Radius.circular(15),),
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1)
-                              )
-                            ),
-                          ),
-                        ),
+                        fieldFormWidget(true,testStirng,"الإسم",modelView.nameIsValid,false,TextInputType.name),
                         SizedBox(height:30),
-                        TextFormField(     
-  
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(
-                            hintText: "اسم المستخدم",
-                            hintTextDirection: TextDirection.rtl,
-                            filled: true,
-                            fillColor: Color(0xffF0F9F1),
-                            enabledBorder:  OutlineInputBorder( 
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1),
-                              ), 
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            focusedBorder: OutlineInputBorder(  
-                              borderRadius: BorderRadius.all(Radius.circular(15),),
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1)
-                              )
-                            ),
-                          ),
-                        ),
+                         fieldFormWidget(false,testStirng,"اسم المستخدم",modelView.nameIsValid,false,TextInputType.name),
                          SizedBox(height:30),
-                        TextFormField(   
-                          keyboardType: TextInputType.emailAddress,  
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(
-                            hintText: "البريد الإلكتروني",
-                            hintTextDirection: TextDirection.rtl,
-                            filled: true,
-                            fillColor: Color(0xffF0F9F1),
-                            enabledBorder:  OutlineInputBorder( 
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1),
-                              ), 
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            focusedBorder: OutlineInputBorder(  
-                              borderRadius: BorderRadius.all(Radius.circular(15),),
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1)
-                              )
-                            ),
-                          ),
-                        ),
+                          fieldFormWidget(false,testStirng,"البريد الإلكتروني",modelView.nameIsValid,false,TextInputType.emailAddress),
                          SizedBox(height:30),
-                        TextFormField(  
-                          obscureText: true,   
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(
-                            hintText: "كلمة المرور ",
-                            hintTextDirection: TextDirection.rtl,
-                            filled: true,
-                            fillColor: Color(0xffF0F9F1),
-                            enabledBorder:  OutlineInputBorder( 
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1),
-                              ), 
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            focusedBorder: OutlineInputBorder(  
-                              borderRadius: BorderRadius.all(Radius.circular(15),),
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1)
-                              )
-                            ),
-                          ),
-                        ),
+                        fieldFormWidget(false,testStirng,"كلمة المرور ",modelView.nameIsValid,true,TextInputType.name),
                          SizedBox(height:30),
-                        TextFormField(     
-                          obscureText: true,
-                          textDirection: TextDirection.rtl,
-                          decoration: InputDecoration(
-                            hintText: "تأكيد كلمة المرور",
-                            hintTextDirection: TextDirection.rtl,
-                            filled: true,
-                            fillColor: Color(0xffF0F9F1),
-                            enabledBorder:  OutlineInputBorder( 
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1),
-                              ), 
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            ),
-                            focusedBorder: OutlineInputBorder(  
-                              borderRadius: BorderRadius.all(Radius.circular(15),),
-                              borderSide: BorderSide(
-                                color: Color(0xffF0F9F1)
-                              )
-                            ),
-                          ),
-                        ),
+                         fieldFormWidget(false,testStirng,"تأكيد كلمة المرور",modelView.nameIsValid,true,TextInputType.name),
                          SizedBox(height:30),
                          Container(
                           width: double.infinity,
@@ -155,7 +55,11 @@ static String singUpFarmer ="/singUpFarmer";
                                 borderRadius: BorderRadius.circular(15)
                               ))
                             ),
-                            onPressed: (){},
+                            onPressed: (){
+                              if (formKay.currentState!.validate()){
+                                  //////write backend code here
+                              }
+                            },
                              child: Text("تسجيل الحساب",
                              style: TextStyle(
                               color: Colors.white,
@@ -194,13 +98,67 @@ static String singUpFarmer ="/singUpFarmer";
                   ),
                 ),
               ),
-             
-          
             ],
           ),
         )
       ),
     );
   }
+//////////////////my widget for this screen///////////////////
 
+Widget fieldFormWidget(bool focased , Function onChanged ,String hintText,
+Function validateor,bool isPassword,TextInputType inputType){
+ 
+                 return  TextFormField(
+
+                          onChanged: (value){
+                            formKay.currentState!.validate();
+                            onChanged(value);
+                          },
+                          keyboardType:inputType,
+                          obscureText:isPassword ,
+                          autofocus: focased,
+                          validator: (value){
+                          return validateor(value);
+                          },
+                          textDirection: TextDirection.rtl,
+                          decoration: InputDecoration(
+                            hintText: hintText,
+                            hintTextDirection: TextDirection.rtl,
+                            filled: true,
+                            fillColor: Color(0xffF0F9F1),
+                            enabledBorder:  OutlineInputBorder( 
+                              borderSide: BorderSide(
+                                color: Color(0xffF0F9F1),
+                              ), 
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
+                            ),
+                            focusedBorder: OutlineInputBorder(  
+                              borderRadius: BorderRadius.all(Radius.circular(15),),
+                              borderSide: BorderSide(
+                                color: Color(0xffF0F9F1)
+                              )
+                            ),
+                            errorBorder: OutlineInputBorder(  
+                              borderRadius: BorderRadius.all(Radius.circular(15),),
+                              borderSide: BorderSide(
+                                color: Colors.red,
+                              ),
+                            ),
+                            focusedErrorBorder:  OutlineInputBorder(  
+                              borderRadius: BorderRadius.all(Radius.circular(15),),
+                              borderSide: BorderSide(
+                                color: Colors.red,
+                              ),
+                            ) 
+                          ),
+                        );
+}
+
+
+//////////////////my widget for this screen///////////////////
+void testStirng(String h){
+
+print(h);
+}
 }
---
+++ lib/modelView/sing_up_farmer.dart
@@ -0,0 +1,17 @@
+class SignUPFarmerModelView {
+
+
+
+String? nameIsValid(String value){
+
+if(value.isEmpty||value==" "){
+  return "name is requaired";
+}
+
+
+
+return null;
+}
+
+
+}
